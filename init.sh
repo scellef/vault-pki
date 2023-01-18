93871af21c6401088914febf3248d793e06438bf
Sean Ellefson <scellef@gmail.com>
2023-01-18 01:16:49 -0800

    Typo

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 vault operator init -format=json -key-shares=1 -key-threshold=1 > vault.init.json
 vault operator unseal $(jq -r .unseal_keys_hex[] < vault.init.json)
-vault login $(jq -r .root_token < vualt.init.json)
+vault login $(jq -r .root_token < vault.init.json)
 
